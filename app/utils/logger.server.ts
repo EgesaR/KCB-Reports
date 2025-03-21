75624e598da84f80881df482095a4d1deb5bae36
Egesa Raymond <111632110+EgesaR@users.noreply.github.com>
2025-03-21 21:52:37 +0000

    Tried something

--- app/utils/logger.server.ts
+++ app/utils/logger.server.ts
@@ -1,16 +1,25 @@
 import pino from "pino";
 
+let transport;
+if (process.env.NODE_ENV !== "production") {
+  try {
+    transport = {
+      target: "pino-pretty",
+      options: {
+        colorize: true,
+        translateTime: "yyyy-mm-dd HH:MM:ss",
+        ignore: "pid,hostname",
+      },
+    };
+  } catch (error) {
+    console.error("Failed to load pino-pretty:", error);
+  }
+}
+
 // Create a Pino logger instance
 const logger = pino({
-  level: process.env.NODE_ENV === "production" ? "info" : "debug", // Log level
-  transport: {
-    target: "pino-pretty", // Pretty print logs in development
-    options: {
-      colorize: true, // Add colors to logs
-      translateTime: "yyyy-mm-dd HH:MM:ss", // Format timestamps
-      ignore: "pid,hostname", // Hide unnecessary fields
-    },
-  },
+  level: process.env.NODE_ENV === "production" ? "info" : "debug",
+  transport,
 });
 
 export default logger;
